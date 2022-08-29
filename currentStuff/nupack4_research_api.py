5a0354b561564bbe2b35f4982c3b89cd4fb76bbd
Jennifer Pearl <jenpearl5@gmail.com>
2022-08-29 13:03:46 -0700

    create pairs dict

--- currentStuff/nupack4_research_api.py
+++ currentStuff/nupack4_research_api.py
@@ -1,13 +1,33 @@
 from nupack import *
 
 
-def getPairProb_all(mySequence, my_model):
+def initializeRnaDict(numNucs):
+    #make the dict and intialize each nuc pair to -1 or not paires. 
+    #ensure that the smae nucs for i and j are retianed for investigations
+    rnaDict={}
+    for i_index in range(numNucs):
+        rnaDict[i_index]={}
+        for y_index in range(numNucs):
+            rnaDict[i_index][y_index]=-1
+
+    return rnaDict
+
+
+def getPairProbsDict(mySequence, my_model):
+    #get pairs array
+    pairsDict =initializeRnaDict(len(mySequence))
     pairsMatrix = pairs(strands=mySequence, model=my_model)
     pairsArray = pairsMatrix.to_array()
-    return pairsArray
 
-def getPairProb_individual(i, j, pairsArray):
-    individualProb = pairsArray[i][j]
+    #now populate the pairs dict
+    for i in range(len(pairsArray)):
+        for j in range(len(pairsArray[i])):
+            pairsDict[i][j]=pairsArray[i][j]
+
+    return pairsDict
+
+def getPairProb_individual(i, j, pairsDict):
+    individualProb = pairsDict[i][j]
     return individualProb
 
 

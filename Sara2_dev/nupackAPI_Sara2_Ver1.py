1e4d4f6e079d9b70350b030413b8a5bf2070d7c4
Jennifer Pearl <jenpearl5@gmail.com>
2023-02-11 08:52:58 +0000

    added ability to process EV for a range and increments

--- Sara2_dev/nupackAPI_Sara2_Ver1.py
+++ Sara2_dev/nupackAPI_Sara2_Ver1.py
@@ -4,6 +4,8 @@ from nupack import *
 import pandas as pd
 import sys
 import openpyxl
+from copy import deepcopy
+from dataclasses import dataclass
 
 my_model = Model
 
@@ -67,31 +69,66 @@ class Sara2StructureList(object):
         self._max_stackEnergy: float = 0
         self._num_structures: int = 0
         self._nuc_count: int = 0
+        self._mfe_structure: str = ''
+        self._mfe_freeEnergy: float = 0
+        self._mfe_stackEnergy: float = 0
+        self._freeEnergy_span:float = 0
+        self._stackEnergy_span:float = 0
+
+    def process_energy(self):
+            #now populate min and max
+        #do free energy
+        self._min_freeEnergy = min(self._freeEnergy_list)
+        self._max_freeEnergy = max(self._freeEnergy_list)
+        self._freeEnergy_span = self._max_freeEnergy - self._min_freeEnergy
+        #do stack energy
+        self._min_stackEnergy = min(self._stackEnergy_list)
+        self._max_stackEnergy = max(self._stackEnergy_list)
+        self._stackEnergy_span = self._max_stackEnergy - self._min_stackEnergy
+
+        #now count
+        self._num_structures = len(self._structures)
 
     def add_structure(self, structure: Sara2SecondaryStructure):
         self._structures.append(structure.structure)
         self._freeEnergy_list.append(structure.freeEnergy)
         self._stackEnergy_list.append(structure.stackEnergy)
+        self.process_energy()
 
-        #now populate min and max
-        #do free energy
-        self._min_freeEnergy = min(self._freeEnergy_list)
-        self._max_freeEnergy = max(self._freeEnergy_list)
-        #do stack energy
-        self._min_stackEnergy = min(self._stackEnergy_list)
-        self._max_stackEnergy = max(self._stackEnergy_list)
 
-        #now count
-        self._num_structures = len(self._structures)
-        self._nuc_count = structure.nuc_count
+    def remove_structure(self, index:int):
+        del self._structures[index]
+        del self._freeEnergy_list[index]
+        del self._stackEnergy_list[index]
+        self.process_energy()            
+
+
+    @property
+    def mfe_structure(self):
+        return self._structures[0].sequence
+
+    @property
+    def mfe_freeEnergy(self):
+        return self._structures[0].freeEnergy
     
+    @property
+    def mfe_stackEnergy(self):
+        return self._structures[0].stackEnergy
+    
+    @property
+    def nuc_count(self):
+        return self._structures[0].nuc_count 
+
     @property
     def stuctures(self):
         return self._structures
 
     @stuctures.setter
     def stuctures(self, structs_list: List[Sara2SecondaryStructure]):
-        self._structures = structs_list
+        self._structures =  deepcopy(structs_list)
+        #now populate min and max
+        #do free energy
+        self.process_energy()
     
     @property
     def max_free_energy(self):
@@ -114,10 +151,13 @@ class Sara2StructureList(object):
         return self._num_structures
     
     @property
-    def nuc_count(self):
-        return self._nuc_count   
+    def freeEnergy_span(self):
+        return self._freeEnergy_span
 
-    
+    @property
+    def stackEnergy_span(self):
+        return self._stackEnergy_span 
+
 
 
 def SetModel(rna_model, temp_C):
@@ -140,8 +180,24 @@ def GetPairProbs2DArray(mySequence, rna_model, temp_C ):
             nucpairs[i].append(pairValue) 
     return nucpairs
 
-class EnsembleVariation:
 
+
+@dataclass
+class EV:
+    ev_normalized: float = -1
+    ev_ThresholdNorm: float = -1
+    ev_structure: float = -1
+
+@dataclass
+class EVResult:
+    groups_list : List[Sara2StructureList] = []
+    groups_dict: Dict[int, Sara2StructureList] = {}
+    group_values: List[float] = []
+    group_ev_list: List[EV] = []
+    group_ev_dict: Dict[int,EV] = {}
+
+
+class EnsembleVariation:
 
     def __init__(self) -> None:
         pass    
@@ -150,7 +206,62 @@ class EnsembleVariation:
         #process ensemble variation
         pass
 
-    def get_subopt_energy_gap(sequence_string, energy_delta_from_MFE: int):
+    def process_ensemble_variation(self, sequence:str, kcal_delta_span_from_mfe:int, Kcal_unit_increments: int):
+        span_structures: Sara2StructureList = self.get_subopt_energy_gap(sequence_string=sequence, energy_delta_from_MFE=kcal_delta_span_from_mfe)       
+        mfe_energy:float =  span_structures.mfe_freeEnergy
+
+        num_groups: int = kcal_delta_span_from_mfe / Kcal_unit_increments
+        remainder: int = kcal_delta_span_from_mfe % Kcal_unit_increments
+
+        
+
+        groups_list : List[Sara2StructureList] = []
+        groups_dict: Dict[int, Sara2StructureList] = {}
+        group_values: List[float] = []
+        group_ev_list: List[EV] = []
+        group_ev_dict: Dict[int,EV] = {}
+
+        #this fills up the list of energy deltas to publich EV's for
+        current_energy: float = mfe_energy
+        for index in num_groups:
+            num = current_energy + Kcal_unit_increments
+            group_values.append(num)
+        
+        if remainder > 0:
+            num = mfe_energy + kcal_delta_span_from_mfe
+            group_values.append(num)
+
+        #now initialize the groups_list
+        for index in range(len(group_values)):
+            group: Sara2StructureList = Sara2StructureList()
+            groups_list.append(group)
+            groups_dict[index+1] = group
+
+
+        for structure in span_structures.stuctures:
+            current_energy = structure.freeEnergy
+
+            #need to do this because there are two indexes need to look at each 
+            #loop and want to avoid triggering a list index overrun
+            for group_index in range(len(group_values)-1):
+                if current_energy in range(group_values[group_index], group_values[group_index+1]):
+                    groups_list[group_index].add_structure(structure)        
+                
+        for group_index in range(len(groups_list)):
+            groups_dict[group_index+1].stuctures = groups_list[group_index]
+
+        #now process all the groups
+        for list_index in range(len(groups_list)):
+            struct_list: Sara2StructureList = groups_list[list_index]
+            ev: EV = self.advanced_EV(struct_list, struct_list.mfe_structure)
+            group_ev_list.append(ev)
+            group_ev_dict[list_index+1] = ev
+
+        result: EVResult = EVResult(groups_list=groups_list, groups_dict=groups_dict, group_values=group_values, group_ev_list=group_ev_list, group_ev_dict=group_ev_dict)
+        return result
+
+
+    def get_subopt_energy_gap(self, sequence_string, energy_delta_from_MFE: int):
         #run through subopt 
         kcal_group_structures_list: Sara2StructureList = Sara2StructureList()
         ensemble_kcal_group= subopt(strands=sequence_string, model=my_model, energy_gap=energy_delta_from_MFE)
@@ -168,7 +279,7 @@ class EnsembleVariation:
         return kcal_group_structures_list
 
     
-    def get_AdvancedEV(kcal_group_structures_list: Sara2StructureList, mfestructure:Sara2SecondaryStructure):
+    def advanced_EV(self, kcal_group_structures_list: Sara2StructureList, mfestructure:Sara2SecondaryStructure):
         #need to do each char abd then structure
         #walk through each nucleotide but first prep containers grab what is needed
         
@@ -204,6 +315,9 @@ class EnsembleVariation:
         # original classic EV and normalized classic EV        
         nucleotide_ensemble_variance_normalized=total_subscore * 100 #/nuc_count    
         fail_threshold=0.2
-        nucleotide_ensemble_variance_ThresholdNorm=(total_subscore/fail_threshold)#*100        
-        return nucleotide_ensemble_variance_normalized, nucleotide_ensemble_variance_ThresholdNorm, structure_ensemble_variance
+        nucleotide_ensemble_variance_ThresholdNorm=(total_subscore/fail_threshold)#*100
+        result: EnsembleVariation.EV =  EnsembleVariation.EV(ev_normalized=nucleotide_ensemble_variance_normalized, 
+                                                            ev_ThresholdNorm=nucleotide_ensemble_variance_ThresholdNorm, 
+                                                            ev_structure=structure_ensemble_variance)      
+        return  result
 
